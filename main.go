989a8cd5bd66f959b7b4b7e039b5fc0f366934fa
Eithel <tux.eithel@gmail.com>
2017-04-26 21:56:44 +0200

    run the web server

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 const (
 	mainFeedURL = "https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json"
 	sourcesURL  = "https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&type_info=1"
 )
 
 func main() {
+
+	routeTemplates := fillTemplate()
+
+	for _, tmpl := range routeTemplates {
+		http.Handle("/"+tmpl.url, tmpl)
+	}
+
+	fmt.Println("open with browser: http://localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
 
 }
