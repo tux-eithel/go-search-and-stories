5dae3e0c927b0c213f289163649ec2a3024a5f63
Eithel <tux.eithel@gmail.com>
2017-04-26 13:57:27 +0200

    initial commit: define some basic urls

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	mainFeedURL = "https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json"
+	sourcesURL  = "https://watrcoolr.duckduckgo.com/watrcoolr.js?o=json&type_info=1"
+)
+
+func main() {
+
+}
