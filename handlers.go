85fea5dfac783b43211948364cab6137d51f6c04
Eithel <tux.eithel@gmail.com>
2017-04-27 14:02:40 +0200

    expand the handler:  - in homepage show my sources' news  - in settings add form to update prefered sources

--- handlers.go
+++ handlers.go
@@ -4,22 +4,46 @@ import "net/http"
 
 func indexHandler(rt *routeTemplate, w http.ResponseWriter, r *http.Request) {
 
+	preparedURL := filterSources(mainFeedURL, preferSources)
+
+	var list []*news
+	var err error
+	if preparedURL != nil {
+		list, err = getNews(preparedURL.String())
+	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if rt.tmpl != nil {
-		rt.tmpl.Execute(w, nil)
+		rt.tmpl.Execute(w, map[string]interface{}{
+			"news":  list,
+			"error": err,
+		})
 	}
 
 }
 
 func indexSettings(rt *routeTemplate, w http.ResponseWriter, r *http.Request) {
 
-	list, err := getSources(sourcesURL)
+	var err error
+	var list []*feed
+
+	if r.Method == "POST" {
+		err = r.ParseForm()
+		if err == nil {
+			preferSources = r.Form["sources"]
+		}
+	}
+
+	if err == nil {
+		list, err = getSources(sourcesURL)
+	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if rt.tmpl != nil {
 		rt.tmpl.Execute(w, map[string]interface{}{
-			"sources": list,
-			"error":   err,
+			"sources":   list,
+			"error":     err,
+			"mysources": preferSources,
 		})
 	}
 }
--- html_templates.go
+++ html_templates.go
@@ -24,6 +24,25 @@ const (
 
 	tplIndex = `{{ define "page" }}
 	<h1>The index page</h1>
+	{{ with .error }}
+		<h3>Some error occurs:</h3>
+<pre>
+{{ . }}
+</pre>
+	{{ end }}
+
+	{{ with .news }}
+		<h2>List of News:</h2>
+		{{ range . }}
+		<div>
+		<h4>{{ .Title }}</h4>
+		<img src="{{ .Image }}" height="300px" width="300px" />
+		<a href="{{ .URL }}" title="see article" target="_blank"/>Read Article</a>
+		</div>
+		{{ end }}
+	{{ end }}
+
+	
 {{ end }}`
 
 	tplSettings = `{{ define "page" }}
@@ -34,13 +53,18 @@ const (
 </pre>
 	{{ end }}
 
+	{{ $mysources := .mysources }}
 	{{ with .sources }}
 		<h2>List of Sources:</h2>
+		<form action="/settings" method="POST" />
 		{{ range . }}
-			<input type="checkbox" value="{{ .ID }}" id="source_{{ .ID }}"/>
+			{{ $id := .ID }}
+			<input name="sources" type="checkbox" {{ with inarray $mysources $id }}checked="checked"{{ end }} value="{{ .ID }}" id="source_{{ .ID }}"/>
 			<label for="source_{{ .ID }}">{{ .Category }} - {{ .Title }}</label>
 			<br />
 		{{ end }}
+		<input type="submit" value="Save Settings"/>
+		</form>
 	{{ end }}
 
 	
