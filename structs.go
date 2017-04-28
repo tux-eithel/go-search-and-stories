5f1d727eb89b2edb8ec0d61a92128b441c11d779
Eithel <tux.eithel@gmail.com>
2017-04-28 10:24:33 +0200

    add new struct to parse time

--- structs.go
+++ structs.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strings"
+	"time"
+)
+
 type (
 	// feed is a struct for sourcesURL
 	feed struct {
@@ -13,13 +18,42 @@ type (
 
 	// news is a struct for mainFeedURL
 	news struct {
-		Feed     string `json:"feed"`
-		Icon     string `json:"favicon"`
-		Image    string `json:"image"`
-		Type     string `json:"type"`
-		Proof    int    `json:"-"`
-		URL      string `json:"url"`
-		Title    string `json:"title"`
-		Category string `json:"category"`
+		Feed      string  `json:"feed"`
+		Icon      string  `json:"favicon"`
+		Image     string  `json:"image"`
+		Type      string  `json:"type"`
+		Proof     int     `json:"-"`
+		Timestamp *myTime `json:"timestamp"`
+		URL       string  `json:"url"`
+		Title     string  `json:"title"`
+		Category  string  `json:"category"`
+	}
+
+	myTime struct {
+		time *time.Time
 	}
 )
+
+func (t *myTime) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+
+	dataSplit := strings.Split(string(data), ".")
+
+	parsedTime, err := time.Parse(`"`+"2006-01-02 15:04:05"+`"`, dataSplit[0]+`"`)
+	if err != nil {
+		return err
+	}
+
+	t.time = &parsedTime
+
+	return nil
+}
+
+func (t *myTime) String() string {
+	if t.time != nil {
+		return (t.time).Format("2006-01-02 - 15:04")
+	}
+	return ""
+}
